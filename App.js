8c881bf69343d310cc7ecae4782b0f8159ce7a6c
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-12-30 12:12:15 -0300

    Adding, Configuring & Using Bottom Tabs

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 
-const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
 	return (
 		<NavigationContainer>
 			{/* <Drawer.Navigator initialRouteName='User'> */}
-			<Drawer.Navigator
+			<BottomTab.Navigator
 				screenOptions={{
 					headerStyle: {
 						backgroundColor: '#3c0a6b',
@@ -20,30 +20,36 @@ export default function App() {
 					headerTitleStyle: {
 						fontWeight: 'bold',
 					},
-					drawerActiveBackgroundColor: '#f0e1ff',
-					drawerActiveTintColor: '#3c0a6b',
-					drawerStyle: {
-						backgroundColor: '#fff',
-					},
+          tabBarActiveTintColor: '#3c0a6b',
 				}}
 			>
-				<Drawer.Screen
+				<BottomTab.Screen
 					name="Welcome"
 					component={WelcomeScreen}
 					options={{
-						// Only apply to Welcome Screen
-						drawerLabel: 'Welcome Screen',
-						drawerIcon: ({ color, size }) => (
-							<Ionicons name="home" color={color} size={size} />
+						tabBarIcon: ({ color, size }) => (
+							<Ionicons
+								name="home"
+								color={color}
+								size={size}
+							/>
 						),
 					}}
 				/>
-				<Drawer.Screen name="User" component={UserScreen} options={{
-          drawerIcon: ({ color, size }) => (
-            <Ionicons name="person" color={color} size={size} />
-          )
-        }}/>
-			</Drawer.Navigator>
+				<BottomTab.Screen
+					name="User"
+					component={UserScreen}
+					options={{
+						tabBarIcon: ({ color, size }) => (
+							<Ionicons
+								name="person"
+								color={color}
+								size={size}
+							/>
+						),
+					}}
+				/>
+			</BottomTab.Navigator>
 		</NavigationContainer>
 	);
 }
--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,18 +1,12 @@
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 function UserScreen({ navigation }) {
-	const openDrawerHandler = () => {
-		navigation.toggleDrawer();
-	};
 
 	return (
 		<View style={styles.rootContainer}>
 			<Text>
 				This is the <Text style={styles.highlight}>"User"</Text> screen!
 			</Text>
-			<View style={{ marginTop: 20 }}>
-				<Button title="Open Drawer" onPress={openDrawerHandler} />
-			</View>
 		</View>
 	);
 }
